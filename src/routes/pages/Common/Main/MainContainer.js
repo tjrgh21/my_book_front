b6466d39234fc0e8330a1ba44a423d909ea98afb
HeoGwan <20191610@g.dongseo.ac.kr>
2024-03-17 16:18:24 +0900

    feat : example

--- src/routes/pages/Common/Main/MainContainer.js
+++ src/routes/pages/Common/Main/MainContainer.js
@@ -4,9 +4,11 @@ import MainPresenter from "./MainPresenter";
 
 const MainContainer = () => {
     const [text, setText] = useState('');
+    const [isSignUp, setIsSignUp] = useState(null);
+    const [isSignIn, setIsSignIn] = useState(null);
 
     const buttonClick = async () => {
-        const result = await fetch('http://localhost:3333/user/signup', {
+        const result = await fetch('http://localhost:3333/test', {
             method: 'get',
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
@@ -20,11 +22,49 @@ const MainContainer = () => {
         setText(data.data);
     }
 
+    const SignUp = async () => {
+        const result = await fetch('http://localhost:3333/user/signup', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                Accept: 'application/json',
+                mode: 'no-cors',
+                'Access-Control-Allow-Origin': '*',
+            }
+        });
+
+        const data = await result.json();
+        console.log(data)
+        setIsSignUp(data)
+    }
+
+    const SignIn = async () => {
+        const result = await fetch('http://localhost:3333/user/signin', {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                Accept: 'application/json',
+                mode: 'no-cors',
+                'Access-Control-Allow-Origin': '*',
+            }
+        });
+
+        const data = await result.json();
+        console.log(data)
+        setIsSignIn(data)
+    }
+
     return (
         <MainPresenter
             text={text}
 
             buttonClick={buttonClick}
+
+            SignUp={SignUp}
+            isSignUp={isSignUp}
+
+            SignIn={SignIn}
+            isSignIn={isSignIn}
         />
     )
 }
--- src/routes/pages/Common/Main/MainPresenter.jsx
+++ src/routes/pages/Common/Main/MainPresenter.jsx
@@ -2,12 +2,24 @@ const MainPresenter = ({
     text,
 
     buttonClick,
+
+    SignUp,
+    isSignUp,
+
+    SignIn,
+    isSignIn,
 }) => {
     return (
         <div>
             <button onClick={buttonClick}>데이터 요청</button>
 
             <div>요청한 데이터: {text}</div>
+
+            <button onClick={SignUp}>회원가입</button>
+            <div>회원가입: {isSignUp}</div>
+
+            <button onClick={SignIn}>로그인</button>
+            <div>로그인: {isSignIn ? '로그인 성공' : '로그인 실패'}</div>
         </div>
     )
 }
