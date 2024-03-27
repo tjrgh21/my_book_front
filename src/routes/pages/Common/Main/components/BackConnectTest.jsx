acb1ab8fbf55003b92956678f88ed1360ae773fc
HeoGwan <20191610@g.dongseo.ac.kr>
2024-03-27 16:21:51 +0900

    feat : modifying explain

--- src/routes/pages/Common/Main/components/BackConnectTest.jsx
+++ src/routes/pages/Common/Main/components/BackConnectTest.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './BackConnectTest.css';
 
+/*
+    ※ 컴포넌트 이름(함수 이름)은 무조건 대문자로 시작해야 함!! ※
+      만약 소문자로 시작한다면 컴포넌트를 출력할 수 없으므로 반드시 주의해서 작성할 것
+*/
 export const BackConnectTest = ({
     buttonClick,
     text,
